c96b3e7c26fad27ab4690d51a7ec69ff07b877cf
Ben White <bwhite@prclosings.com>
2013-11-08 16:57:20 -0800

    Added some controls to the login page and updated the stylesheet

--- PTCOrderSite/Login.aspx
+++ PTCOrderSite/Login.aspx
@@ -2,4 +2,31 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="body" runat="server">
+
+<div class="formBox loginBox">
+    <asp:Table runat="server" ID="tblLogin">
+        <asp:TableRow runat="server">
+            <asp:TableCell runat="server">
+                <asp:Label runat="server" ID="lblLogin" Text="Username:"></asp:Label>
+            </asp:TableCell>
+            <asp:TableCell runat="server">
+                <asp:TextBox runat="server" ID="txtLogin"></asp:TextBox>
+            </asp:TableCell>
+        </asp:TableRow>
+        <asp:TableRow ID="TableRow1" runat="server">
+            <asp:TableCell ID="TableCell1" runat="server">
+                <asp:Label runat="server" ID="lblPassword" Text="Password:"></asp:Label>
+            </asp:TableCell>
+            <asp:TableCell ID="TableCell2" runat="server">
+                <asp:TextBox runat="server" ID="txtPassword"></asp:TextBox>
+            </asp:TableCell>
+        </asp:TableRow>
+        <asp:TableRow runat="server">
+            <asp:TableCell runat="server" ColumnSpan=2>
+                <asp:Button runat="server" ID="cmdSubmit" Text="Login" />
+            </asp:TableCell>
+        </asp:TableRow>
+    </asp:Table>
+</div>
+
 </asp:Content>
--- PTCOrderSite/Login.aspx.designer.cs
+++ PTCOrderSite/Login.aspx.designer.cs
@@ -11,5 +11,86 @@ namespace PTCOrderSite {
     
     
     public partial class Login {
+        
+        /// <summary>
+        /// tblLogin control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Table tblLogin;
+        
+        /// <summary>
+        /// lblLogin control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblLogin;
+        
+        /// <summary>
+        /// txtLogin control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtLogin;
+        
+        /// <summary>
+        /// TableRow1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableRow TableRow1;
+        
+        /// <summary>
+        /// TableCell1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableCell TableCell1;
+        
+        /// <summary>
+        /// lblPassword control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblPassword;
+        
+        /// <summary>
+        /// TableCell2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TableCell TableCell2;
+        
+        /// <summary>
+        /// txtPassword control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtPassword;
+        
+        /// <summary>
+        /// cmdSubmit control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button cmdSubmit;
     }
 }
